7ff67fb0acc58a4c950e55260f8d86f33306ef5b
asreet <ramanpreetkaur705@gmail.com>
2021-02-09 16:13:00 +0530

    new project

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import{createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation'
+
+import TransactionScreen from './screens/transactionScreen';
+import searchScreen from './screens/searchScreen';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer />
+    );
+  }
 }
+const TabNavigator = createBottomTabNavigator({
+  Transaction:{screen:TransactionScreen},
+  Search:{screen:searchScreen}
+},
+{
+  defaultNavigatorOptions : ({navigation}) =>({
+    tabBarIcon:({})=>{
+      const routeName=navigation.state.routeName
+      if(routeName=== 'Transaction'){
+        return(
+          <Image
+          source={require('./assets/book.png')}
+          style = {{ width:40,height:40}}/>
+        )
+      }
+      else if(routeName==='Search'){
+        return(
+          <Image
+          source={require('./assets/searchingbook.png')}
+          style = {{ width:40,height:40}}/>
+        )
+      }
+    }
+  })
+});
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
